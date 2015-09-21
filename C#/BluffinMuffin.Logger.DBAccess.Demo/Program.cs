8a6332cbcc17fd5d76434ce54b0a8c6723b50b6f
Éric Massé <Ericmas001@gmail.com>
2015-09-21 13:49:47 -0400

    Logging commands

--- C#/BluffinMuffin.Logger.DBAccess.Demo/Program.cs
+++ C#/BluffinMuffin.Logger.DBAccess.Demo/Program.cs
@@ -20,9 +20,21 @@ namespace BluffinMuffin.Logger.DBAccess.Demo
             var client1 = new Client("BluffinMuffin.Logger.DBAccess.Demo", new Version(3, 0, 0), "127.0.0.1");
             client1.RegisterClient();
 
+            var c1 = new Command("CheckCompatibilityCommand", server,client1, "{  \"CommandName\": \"CheckCompatibilityCommand\",  \"ImplementedProtocolVersion\": \"2.0.0\"}");
+            c1.RegisterLobbyCommandFromClient();
+
+            var c1R = new Command("CheckCompatibilityResponse", server,client1, "{  \"CommandName\": \"CheckCompatibilityResponse\",  \"Success\": true,  \"MessageId\": \"None\",  \"Message\": \"\",  \"ImplementedProtocolVersion\": \"2.0.0\",  \"SupportedLobbyTypes\": [    \"QuickMode\",    \"RegisteredMode\"  ],  \"AvailableGames\": [    {      \"GameType\": \"CommunityCardsPoker\",      \"AvailableVariants\": [        \"TexasHoldem\",        \"OmahaHoldem\",        \"CrazyPineapple\"      ],      \"AvailableLimits\": [        \"NoLimit\",        \"FixedLimit\",        \"PotLimit\"      ],      \"AvailableBlinds\": [        \"Blinds\", \"Antes\", \"None\"      ],      \"MinPlayers\": 2,      \"MaxPlayers\": 10    }  ],  \"Command\": {    \"CommandName\": \"CheckCompatibilityCommand\",    \"ImplementedProtocolVersion\": \"2.0.0\"  }}");
+            c1R.RegisterLobbyCommandFromServer();
+
             var client2 = new Client("BluffinMuffin.Logger.DBAccess.Demo", new Version(3, 0, 0), "127.0.0.1");
             client2.RegisterClient();
             client2.Identify("SpongeBob");
+
+            var c2 = new Command("CheckCompatibilityCommand", server, client2, "{  \"CommandName\": \"CheckCompatibilityCommand\",  \"ImplementedProtocolVersion\": \"2.0.0\"}");
+            c2.RegisterLobbyCommandFromClient();
+
+            var c2R = new Command("CheckCompatibilityResponse", server, client2, "{  \"CommandName\": \"CheckCompatibilityResponse\",  \"Success\": true,  \"MessageId\": \"None\",  \"Message\": \"\",  \"ImplementedProtocolVersion\": \"2.0.0\",  \"SupportedLobbyTypes\": [    \"QuickMode\",    \"RegisteredMode\"  ],  \"AvailableGames\": [    {      \"GameType\": \"CommunityCardsPoker\",      \"AvailableVariants\": [        \"TexasHoldem\",        \"OmahaHoldem\",        \"CrazyPineapple\"      ],      \"AvailableLimits\": [        \"NoLimit\",        \"FixedLimit\",        \"PotLimit\"      ],      \"AvailableBlinds\": [        \"Blinds\", \"Antes\", \"None\"      ],      \"MinPlayers\": 2,      \"MaxPlayers\": 10    }  ],  \"Command\": {    \"CommandName\": \"CheckCompatibilityCommand\",    \"ImplementedProtocolVersion\": \"2.0.0\"  }}");
+            c2R.RegisterLobbyCommandFromServer();
 
             var table = new Table("Bikini Bottom", GameSubTypeEnum.TexasHoldem, 2,10,BlindTypeEnum.Blinds, LobbyTypeEnum.QuickMode, LimitTypeEnum.NoLimit, server);
             table.RegisterTable();
---
+++ C#/BluffinMuffin.Logger.DBAccess/Command.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using BluffinMuffin.Logger.DBAccess.Enums;
+
+namespace BluffinMuffin.Logger.DBAccess
+{
+    public class Command
+    {
+        private const string GENERAL = "GENERAL";
+        private const string LOBBY = "LOBBY";
+        private const string GAME = "GAME";
+
+        internal int Id { get; private set; }
+
+        public string Name { get; }
+        public Server Server { get; }
+        public Client Client { get; }
+        public string Detail { get; }
+
+        public Command(string name, Server server, Client client, string detail)
+        {
+            Name = name;
+            Server = server;
+            Client = client;
+            Detail = detail;
+        }
+
+        private void RegisterCommand(bool isFromServer, string type)
+        {
+            if (Id > 0)
+                return;
+
+            using (var context = Database.GetContext())
+            {
+                var client = context.AllClients.Single(x => x.Id == Client.Id);
+                var server = context.AllServers.Single(x => x.Id == Server.Id);
+                var c = new CommandEntity()
+                {
+                    Name = Name,
+                    Detail = Detail,
+                    IsFromServer = isFromServer,
+                    Type = type,
+                    Server = server,
+                    Client = client,
+                    ExecutionTime = DateTime.Now
+                };
+                context.AllCommands.Add(c);
+                context.SaveChanges();
+                Id = c.Id;
+            }
+        }
+
+        public void RegisterGeneralCommandFromServer()
+        {
+            RegisterCommand(true, GENERAL);
+        }
+
+        public void RegisterGeneralCommandFromClient()
+        {
+            RegisterCommand(false, GENERAL);
+        }
+
+        public void RegisterLobbyCommandFromServer()
+        {
+            RegisterCommand(true, LOBBY);
+        }
+
+        public void RegisterLobbyCommandFromClient()
+        {
+            RegisterCommand(false, LOBBY);
+        }
+
+        public void RegisterGameCommandFromServer()
+        {
+            RegisterCommand(true, GAME);
+        }
+
+        public void RegisterGameCommandFromClient()
+        {
+            RegisterCommand(false, GAME);
+        }
+    }
+}
